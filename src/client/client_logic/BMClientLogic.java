582ac8e847357161375ca3cb934197f03e4e4007
troombox <45360723+troombox@users.noreply.github.com>
2021-12-08 03:07:41 +0200

    Add files via upload

--- src/client/client_logic/BMClientLogic.java
+++ src/client/client_logic/BMClientLogic.java
@@ -1,5 +1,35 @@
 package client.client_logic;
 
-public class BMClientLogic {
+import java.io.IOException;
 
+import ocsf.client.AbstractClient;
+import utility.enums.DataType;
+import utility.enums.RequestType;
+
+public class BMClientLogic extends AbstractClient{
+	
+	public BMClientLogic(String host, int port) throws IOException {
+	    super(host, port); //Call the superclass constructor
+	    openConnection();
+	    System.out.println("connection opened");
+	}
+
+	@Override
+	protected void handleMessageFromServer(Object msg) {
+		// TODO Auto-generated method stub
+	}
+	
+	public void sendMessageToServer(Object dataToSendToServer, DataType dataType, RequestType requestType) {
+		
+	}
+	
+	private void handleMessageToServer(Object msg) {
+		try {
+			sendToServer(msg);
+		} catch (IOException e) {
+			System.out.println("Could not send message to server.  Terminating client.");
+			System.exit(1);
+		}
+	}
+	
 }
