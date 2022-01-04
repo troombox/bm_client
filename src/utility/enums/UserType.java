c23c0656c2d1b5df84cc87caaefcafa7c2f7a7e6
edenamir <40493066+edenamir@users.noreply.github.com>
2022-01-04 10:59:00 +0200

    Add files via upload

--- src/utility/enums/UserType.java
+++ src/utility/enums/UserType.java
@@ -1,27 +1,63 @@
 package utility.enums;
 
+/**
+ * The Enum UserType.
+ *  represent all tpye of users in system.
+ */
 public enum UserType {
 	
+	/** The user. */
 	USER("USER"),
+	
+	/** The client personal. */
 	CLIENT_PERSONAL("CLIENT_PERSONAL"),
+	
+	/** The client business. */
 	CLIENT_BUSINESS("CLIENT_BUSINESS"),
+	
+	/** The hr manager. */
 	HR_MANAGER("HR_MANAGER"),
+	
+	/** The bm branch manager. */
 	BM_BRANCH_MANAGER("BM_BRANCH_MANAGER"), 
+	
+	/** The bm ceo. */
 	BM_CEO("BM_CEO"), 
+	
+	/** The restaurant owner. */
 	RESTAURANT_OWNER("RESTAURANT_OWNER"), 
+	
+	/** The restaurant trusted worker. */
 	RESTAURANT_TRUSTED_WORKER("RESTAURANT_TRUSTED_WORKER");
 
+	/** The user type. */
 	private String userType;
 
+	/**
+	 * Instantiates a new user type.
+	 *
+	 * @param userType the user type
+	 */
 	private UserType(String userType) {
 		this.userType = userType;
 	}
 
+	/**
+	 * To string.
+	 *
+	 * @return the userType string
+	 */
 	@Override
 	public String toString() {
 		return userType;
 	}
 	
+	/**
+	 * From string.
+	 *
+	 * @param userTypeString the user type string
+	 * @return the user type 
+	 */
 	public static UserType fromString(String userTypeString) {
 		switch(userTypeString) {
 		case "CLIENT_PERSONAL":
