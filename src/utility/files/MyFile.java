34d75c385a667b297c288b2de570944cb872c279
Karin-sh <72125356+Karin-sh@users.noreply.github.com>
2022-01-05 01:08:59 +0200

    javadoc to utillity.files
    javadoc to utillity.files

--- src/utility/files/MyFile.java
+++ src/utility/files/MyFile.java
@@ -2,58 +2,124 @@ package utility.files;
 
 import java.io.Serializable;
 
+/**
+ * The Class MyFile.
+ */
 public class MyFile implements Serializable {
 	
+	/** The Description. */
 	private String Description=null;
+	
+	/** The file name. */
 	private String fileName=null;	
+	
+	/** The size. */
 	private int size=0;
+	
+	/** The mybytearray. */
 	public  byte[] mybytearray;
 	
 	
+	/**
+	 * Inits the array.
+	 *
+	 * @param size the size
+	 */
 	public void initArray(int size)
 	{
 		mybytearray = new byte [size];	
 	}
 	
+	/**
+	 * Instantiates a new my file.
+	 *
+	 * @param fileName the file name
+	 */
 	public MyFile( String fileName) {
 		this.fileName = fileName;
 	}
 	
 	
+	/**
+	 * Gets the file name.
+	 *
+	 * @return the file name
+	 */
 	public String getFileName() {
 		return fileName;
 	}
 
+	/**
+	 * Sets the file name.
+	 *
+	 * @param fileName the new file name
+	 */
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
 	
+	/**
+	 * Gets the size.
+	 *
+	 * @return the size
+	 */
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	 * Sets the size.
+	 *
+	 * @param size the new size
+	 */
 	public void setSize(int size) {
 		this.size = size;
 	}
 
+	/**
+	 * Gets the mybytearray.
+	 *
+	 * @return the mybytearray
+	 */
 	public byte[] getMybytearray() {
 		return mybytearray;
 	}
 	
+	/**
+	 * Gets the mybytearray.
+	 *
+	 * @param i the i
+	 * @return the mybytearray
+	 */
 	public byte getMybytearray(int i) {
 		return mybytearray[i];
 	}
 
+	/**
+	 * Sets the mybytearray.
+	 *
+	 * @param mybytearray the new mybytearray
+	 */
 	public void setMybytearray(byte[] mybytearray) {
 		
 		for(int i=0;i<mybytearray.length;i++)
 		this.mybytearray[i] = mybytearray[i];
 	}
 
+	/**
+	 * Gets the description.
+	 *
+	 * @return the description
+	 */
 	public String getDescription() {
 		return Description;
 	}
 
+	/**
+	 * Sets the description.
+	 *
+	 * @param description the new description
+	 */
 	public void setDescription(String description) {
 		Description = description;
 	}	
