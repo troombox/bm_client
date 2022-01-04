e60a06d1693f8d459b4ee55f7c9b0a82f38df3cb
edenamir <40493066+edenamir@users.noreply.github.com>
2022-01-04 10:50:15 +0200

    Add files via upload

--- src/utility/enums/OrderType.java
+++ src/utility/enums/OrderType.java
@@ -1,23 +1,54 @@
 package utility.enums;
 
+/**
+ * The Enum OrderType.
+ * represent all actions related to order type in system.
+ */
 public enum OrderType {
+	
+	/** The pickup. */
 	PICKUP("PICKUP"),
+	
+	/** The delivery early. */
 	DELIVERY_EARLY("DELIVERY_EARLY"),
+	
+	/** The delivery regular. */
 	DELIVERY_REGULAR("DELIVERY_REGULAR"),
+	
+	/** The delivery robot. */
 	DELIVERY_ROBOT("DELIVERY_ROBOT"),
+	
+	/** The unknown. */
 	UNKNOWN("UNKNOWN");
 	
+	/** The order type. */
 	private String orderType;
 	
+	/**
+	 * Instantiates a new order type.
+	 *
+	 * @param orderType the order type
+	 */
 	private OrderType(String orderType) {
 		this.orderType = orderType;
 	}
 	
+	/**
+	 * To string.
+	 *
+	 * @return the orderType string
+	 */
 	@Override
 	public String toString() {
 		return orderType;
 	}
 	
+	/**
+	 * From string.
+	 *
+	 * @param orderTypeString the order type string
+	 * @return the order type
+	 */
 	public static OrderType fromString(String orderTypeString) {
 		switch(orderTypeString) {
 		case "PICKUP":
