c7200d9c4355e04a6cc2b1635235e031f8d6ac5d
troombox <45360723+troombox@users.noreply.github.com>
2022-01-05 01:26:28 +0200

    javadoc for interfaces made by semion

--- src/client/interfaces/IClientFxController.java
+++ src/client/interfaces/IClientFxController.java
@@ -2,6 +2,16 @@ package client.interfaces;
 
 import javafx.stage.Stage;
 
+/**
+ * The Interface IClientFxController is interface implemented by all the windows in our application
+ * 
+ */
 public interface IClientFxController {
+	
+	/**
+	 *  this method starts the FX window controller that implements the IClientFxController interface 
+	 *
+	 * @param stage - main stage of the application
+	 */
 	public void start(Stage stage);
 }
