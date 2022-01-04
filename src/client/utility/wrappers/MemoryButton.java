eee86ff61e59df822231386e7464175b6fb18aa9
troombox <45360723+troombox@users.noreply.github.com>
2022-01-05 01:25:49 +0200

    java doc for wrappers made by semion

--- src/client/utility/wrappers/MemoryButton.java
+++ src/client/utility/wrappers/MemoryButton.java
@@ -2,14 +2,32 @@ package client.utility.wrappers;
 
 import javafx.scene.control.Button;
 
+/**
+ * The Class MemoryButton extends javafx Button class.
+ *	Memory button acts just like a regular javafx button, 
+ *with an addition of remembering one index given in constructor
+ */
 public class MemoryButton extends Button{
+	
+	/** The index saved. */
 	private int index;
 	
+	/**
+	 * Instantiates a new memory button.
+	 *
+	 * @param text - the text to be shown on button
+	 * @param index - the index to remember
+	 */
 	public MemoryButton(String text, int index) {
 		super(text);
 		this.index = index;
 	}
 	
+	/**
+	 * Gets the index saved
+	 *
+	 * @return the index - variable given in constructor
+	 */
 	public int getIndex() {
 		return index;
 	}
--- src/client/utility/wrappers/MultiOrder.java
+++ src/client/utility/wrappers/MultiOrder.java
@@ -7,20 +7,46 @@ import utility.entity.Order;
 import utility.entity.User;
 import utility.enums.OrderType;
 
+/**
+ * The Class MultiOrder - used to keep data about a collection of dishes that 
+ * not necessary belong to same restaurant ID 
+ */
 public class MultiOrder extends Order {
 	
+	/** The array keeping restaurant ID's of dishes saved in the order. */
 	ArrayList<String> resIDs;
 
+	/**
+	 * Instantiates a new multi order.
+	 *
+	 * @param userFirstName - the user first name
+	 * @param userLastName -  the user last name
+	 * @param userPhone - the user phone
+	 * @param deliveryAddress - the delivery address
+	 * @param restaurantID - the restaurant ID of a first dish added
+	 * @param typeOfOrder - the type of order
+	 */
 	public MultiOrder(String userFirstName, String userLastName, String userPhone, String deliveryAddress,
 			int restaurantID, OrderType typeOfOrder) {
 		super(userFirstName, userLastName, userPhone, deliveryAddress, restaurantID, typeOfOrder);
 		this.resIDs = new ArrayList<String>();
 	}
 	
+	/**
+	 * Instantiates a new multi order.
+	 *
+	 * @param user the user
+	 * @param restaurantID the restaurant ID
+	 */
 	public MultiOrder(User user,  int restaurantID) {
 		this(user.getFirstName(), user.getLastName(), user.getPhone(), "", restaurantID, OrderType.UNKNOWN);
 	}
 	
+	/**
+	 * Adds the dish.
+	 *
+	 * @param dish - the dish to add
+	 */
 	@Override
 	public void addDish(Dish dish) {
 		super.addDish(dish);
@@ -29,6 +55,11 @@ public class MultiOrder extends Order {
 		}
 	}
 	
+	/**
+	 * Removes the dish.
+	 *
+	 * @param dish - the dish to remove
+	 */
 	@Override
 	public void removeDish(Dish dish) {
 		super.removeDish(dish);
@@ -41,10 +72,20 @@ public class MultiOrder extends Order {
 		resIDs.remove(dish.getRes_ID());
 	}
 	
+	/**
+	 * Number of restaurants in order.
+	 *
+	 * @return the int value of number of restaurants in order.
+	 */
 	public int numberOfRestaurantsInOrder() {
 		return resIDs.size();
 	}
 	
+	/**
+	 * ArrayList of restaurant ID's saved in the multiorder
+	 *
+	 * @return the array list
+	 */
 	public ArrayList<String> restaurantsInOrderList() {
 		return resIDs;
 	}
